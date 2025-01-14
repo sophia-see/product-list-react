0a999b74c6741d2d06c0420b4f65ab554ae39c6c
Sophia See <sophiasee11@gmail.com>
2025-01-15 02:34:30 +0800

    changed back to strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>
-  ,
+  </StrictMode>,
 )
